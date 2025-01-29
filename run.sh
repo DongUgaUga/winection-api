c70387c2fd7634739c142fb4051ddaf3a88580c3
woodong-222 <dw2083ehddn@naver.com>
2025-01-29 10:05:51 +0900

    Rename: /test_frond -> /test_front 폴더명 변경

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# src 디렉터리를 PYTHONPATH에 추가
+export PYTHONPATH=$(pwd)/src
+
 # 패키지 설치 (이미 설치된 경우 스킵됨)
-pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir -q -r requirements.txt
 
 # FastAPI 서버 실행
 uvicorn src.app:app --host 0.0.0.0 --port 8000 --reload
