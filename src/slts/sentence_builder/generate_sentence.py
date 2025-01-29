1ec3acd2a769cb5a58254416927eed13616b45e9
woodong-222 <dw2083ehddn@naver.com>
2025-01-29 12:31:13 +0900

    Feat: Google TTS api를 서버에 합침

--- src/slts/sentence_builder/generate_sentence.py
+++ src/slts/sentence_builder/generate_sentence.py
@@ -1,21 +1,22 @@
 from openai import OpenAI
 import os
-import logging
 from dotenv import load_dotenv
+import logging
 
 # 환경변수 로드
 load_dotenv()
 
 # API 키 설정
-api_key = os.getenv("DEEPSEEK_API_KEY")
-if not api_key:
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+if not DEEPSEEK_API_KEY:
     raise ValueError("환경변수 'DEEPSEEK_API_KEY'가 설정되지 않았습니다.")
 
+
 # OpenAI 클라이언트 초기화
-client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com")
 
 # DeepSeek API 호출 함수
-def generate_sentence_from_words(words: list) -> str:
+def generate_sentence(words: list) -> str:
     system_message = {
         "role": "system",
         "content": "너는 이제부터 수화를 번역할 거야. 내가 단어들을 던져주면 그걸 자연스러운 하나의 문장으로 만들어줘. 부가적인 설명은 하지 말고, 오로지 하나의 문장만 출력해."
