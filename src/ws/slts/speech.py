1374e34958a6916940c445b1c700773d825802ef
woodong-222 <dw2083ehddn@naver.com>
2025-01-31 11:30:42 +0900

    Feat: 프론트에서 STT과정을 실행 -> 백으로 수행 완료한 텍스트 전달

--- src/ws/slts/speech.py
+++ src/ws/slts/speech.py
@@ -1,47 +1,28 @@
-# Google TTS 음성 변환
 import google.cloud.texttospeech as tts
 import base64
-import os
-from dotenv import load_dotenv
 import logging
-
-# 환경변수 로드
-load_dotenv()
-
-# API 키 설정
-GOOGLETTS_API_KEY = os.getenv("GOOGLETTS_API_KEY")
-if not GOOGLETTS_API_KEY:
-    raise ValueError("환경변수 'GOOGLETTS_API_KEY'가 설정되지 않았습니다.")
-
-# 프로젝트 ID
-PROJECT_ID = "winection-project"
+from ws.keyload import PROJECT_ID  # 인증 자동 적용됨
 
 def text_to_speech(voice_name: str, text: str) -> str:
     try:
-        client = tts.TextToSpeechClient(client_options={"api_key": GOOGLETTS_API_KEY, "quota_project_id": PROJECT_ID})
+        client = tts.TextToSpeechClient()
         language_code = "-".join(voice_name.split("-")[:2])
 
         # 텍스트 입력 설정
         text_input = tts.SynthesisInput(text=text)
 
         # 음성 선택 파라미터
-        voice_params = tts.VoiceSelectionParams(
-            language_code=language_code, name=voice_name
-        )
+        voice_params = tts.VoiceSelectionParams(language_code=language_code, name=voice_name)
 
         # 오디오 생성 설정 (MP3 형식)
         audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.MP3)
 
-        # TTS 호출
-        response = client.synthesize_speech(
-            input=text_input,
-            voice=voice_params,
-            audio_config=audio_config,
-        )
+        # Google TTS API 호출
+        response = client.synthesize_speech(input=text_input, voice=voice_params, audio_config=audio_config)
 
         # 오디오 데이터를 Base64로 인코딩하여 반환
         audio_base64 = base64.b64encode(response.audio_content).decode("utf-8")
-
+        logging.info(f"[Google TTS] 음성 변환 완료: {text}")
         return audio_base64
 
     except Exception as e:
