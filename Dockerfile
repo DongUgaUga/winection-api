d6ef42e73df3aeed79365a0c855fd81ea437c91f
woodong-222 <dw2083ehddn@naver.com>
2025-02-02 02:44:15 +0900

    Fix: Jenkins Secret text로 api키 위치 변경

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,15 @@ FROM python:3.9-slim-buster
 WORKDIR /src
 COPY . .
 
+
+# Build-time 환경변수 추가
+ARG GOOGLE_API_KEY
+ARG DEEPSEEK_API_KEY
+
+# 환경변수 설정
+ENV GOOGLE_CLOUD_API_KEY=$GOOGLE_API_KEY
+ENV DEEPSEEK_API_KEY=$DEEPSEEK_API_KEY
+
 RUN apt update && apt install libpq-dev gcc -y && pip install --no-cache-dir --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 8000
--- src/ws/load.py
+++ src/ws/load.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 # 환경변수 로드
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_CLOUD_API_KEY")
+PROJECT_ID = 'winection-project'
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_CLOUD_API_KEY", os.environ.get("GOOGLE_CLOUD_API_KEY"))
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY", os.environ.get("DEEPSEEK_API_KEY"))
+
 if not GOOGLE_API_KEY:
     raise ValueError("환경변수 'GOOGLE_CLOUD_API_KEY'가 설정되지 않았습니다.")
 
-PROJECT_ID = 'winection-project'
 
-# DeepSeek API 키 (환경 변수에서 로드)
-DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 if not DEEPSEEK_API_KEY:
     raise ValueError("환경변수 'DEEPSEEK_API_KEY'가 설정되지 않았습니다.")
+
