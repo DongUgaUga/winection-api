18ce09c0fff1acb09982cda3a90ef748d377b975
woodong-222 <dw2083ehddn@naver.com>
2025-03-19 22:38:09 +0900

    refactor: 디렉터리 구조 및 파일명 변경

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from core.models import TranslationRequest, TranslationResponse
 from core.logging import logger
-from src.api.to_speech.to_speech_server import to_speech_router
-from src.api.to_sign.to_sign_server import to_sign_router
+from api.to_speech.to_speech_router import to_speech_router
+from api.to_sign.to_sign_router import to_sign_router
 from src.api.to_speech.services.sentence import word_to_sentence
 from src.api.to_speech.services.speech import text_to_speech
 
