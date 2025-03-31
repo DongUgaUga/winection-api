6b05aef2bb35ef91095563221cfc19ba3a2f95b3
woodong-222 <dw2083ehddn@naver.com>
2025-03-31 19:10:53 +0900

    hotfix: db 관련 기능 제외

--- src/core/models.py
+++ src/core/models.py
@@ -3,23 +3,23 @@ from typing import List, Any, Union
 
 from sqlalchemy import Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import mapped_column, relationship
-from .database import Base
+# from .database import Base
 
-class User(Base):
-    __tablename__ = "users"
-    id = mapped_column(Integer, primary_key=True, autoincrement=True)
-    name = mapped_column(String(255), nullable=False)
-    email = mapped_column(String(255), unique=True, nullable=False)
-    posts = relationship("Post",back_populates="owner", cascade='delete')
-    is_active = mapped_column(Boolean,default=False)
+# class User(Base):
+#     __tablename__ = "users"
+#     id = mapped_column(Integer, primary_key=True, autoincrement=True)
+#     name = mapped_column(String(255), nullable=False)
+#     email = mapped_column(String(255), unique=True, nullable=False)
+#     posts = relationship("Post",back_populates="owner", cascade='delete')
+#     is_active = mapped_column(Boolean,default=False)
 
-class Post(Base):
-    __tablename__ = "posts"
-    id = mapped_column(Integer, primary_key=True, autoincrement=True)
-    title = mapped_column(String(255), nullable=False)
-    description = mapped_column(String(255))
-    owner_id = mapped_column(Integer, ForeignKey("users.id"))
-    owner = relationship("User",back_populates="posts")
+# class Post(Base):
+#     __tablename__ = "posts"
+#     id = mapped_column(Integer, primary_key=True, autoincrement=True)
+#     title = mapped_column(String(255), nullable=False)
+#     description = mapped_column(String(255))
+#     owner_id = mapped_column(Integer, ForeignKey("users.id"))
+#     owner = relationship("User",back_populates="posts")
 
 
 # TO_SIGN API
