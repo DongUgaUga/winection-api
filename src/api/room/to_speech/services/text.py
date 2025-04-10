9de09e5c39ea9e0ba3b2535ddaca0ffb80a1832c
woodong-222 <dw2083ehddn@naver.com>
2025-04-10 23:30:33 +0900

    fix: 모델 입력 형태 수정

--- src/api/room/to_speech/services/text.py
+++ src/api/room/to_speech/services/text.py
@@ -1,20 +1,20 @@
-# text.py
-
-import numpy as np
 from tensorflow.keras.models import load_model
+import numpy as np
 
-# 모델 & 클래스명 로드
-model = load_model("src/resources/ksl_cnn_lstm_model.h5")
-class_names = np.load("src/resources/class_names.npy")
+model = load_model("path_to_model/ksl_cnn_lstm_model.h5")
+class_names = np.load("path_to_class_names/class_names.npy")
 
 def ksl_to_korean(sequence: list[list[float]]) -> str:
     try:
         arr = np.array(sequence, dtype=np.float32)
+
         if arr.shape != (60, 225):
-            raise ValueError("입력 shape은 (60, 225) 여야 합니다.")
+            raise ValueError(f"입력 shape은 (60, 225) 여야 합니다. 입력 shape: {arr.shape}")
 
         input_tensor = np.expand_dims(arr, axis=0)  # (1, 60, 225)
+
         pred = model.predict(input_tensor)
+
         label = class_names[np.argmax(pred)]
 
         return str(label)
